223348911a85daa21cbf5cb5468adbffb381572f
bsb99 <wer06099@naver.com>
2024-02-05 08:48:52 +0900

    Study : Spring Batch Chunk

--- src/main/java/com/example/demo/JobConfiguration.java
+++ src/main/java/com/example/demo/JobConfiguration.java
@@ -10,6 +10,10 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,31 +24,34 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class JobConfiguration {
     @Bean
     public Job job(JobRepository jobRepository, Step step) {
-        return new JobBuilder("job", jobRepository) //job name 설정
+        return new JobBuilder("job-chunk", jobRepository) //job name 설정
                 .start(step)
                 .build();
     }
 
     @Bean
     public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
-        // tasklet = Step이 중지될 때 까지 execute 메서드가 계속 반복해서 수행하고 수행할 때 마다 독립적인 트랜잭션이 만들어진다.
-        // 초기화, 저장 프로시저 실행, 알림 전송과 같은 job에서 일반적으로 실행
-        final Tasklet tasklet = new Tasklet() {
+        final ItemReader<Integer> itemReader = new ItemReader<>() {
+
             private int count = 0;
+
             @Override
-            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+            public Integer read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
                 count++;
-                if (count == 15) {
-                    log.info("tasklet FINISHED");
-                    return RepeatStatus.FINISHED;
-                }
-                log.info("tasklet CONTINUABLE {}", count);
-                return RepeatStatus.CONTINUABLE;
+
+                log.info("count {}", count);
+
+                if(count == 15) return null;
+
+                return count;
             }
         };
 
         return new StepBuilder("step", jobRepository)
-                .tasklet(tasklet, platformTransactionManager)
+                .chunk(10, platformTransactionManager)
+                .reader(itemReader)
+//                .processor()
+                .writer(read -> {})
                 .build();
     }
 }
